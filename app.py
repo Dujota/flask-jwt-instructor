1005f6d79a7d0340a79b92d8350ecc698d13d915
Denis S Dujota <denisdujota@gmail.com>
2024-09-03 14:07:36 +0300

    setup sign and verify:

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # app.py
 # Import the 'Flask' class from the 'flask' library.
-from flask import Flask
-
+from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+import os
+import jwt
+load_dotenv()
 # Initialize Flask
 # We'll use the pre-defined global '__name__' variable to tell Flask where it is.
 app = Flask(__name__)
@@ -11,6 +14,33 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return "Hello, world!"
+
+@app.route('/sign-token', methods=['GET'])
+def sign_token():
+    user = {
+        "id": 1,
+        "username": "test",
+        "password": "test"
+    }
+
+    token = jwt.encode(user, os.environ["JWT_SECRET"], algorithm="HS256")
+
+    return jsonify({"token" : token})
+
+
+@app.route('/verify-token', methods=['POST'])
+def verify_token():
+  try:
+    token = request.headers.get("Authorization").split(" ")[1]
+
+    decoded =  jwt.decode(token, os.getenv("JWT_SECRET"), algorithms="HS256")
+
+    return jsonify({"user" : decoded})
+  except Exception:
+    return jsonify({"error" : "Invalid token"}), 401
+
+
+
 
 # Run our application, by default on port 5000
 app.run()
